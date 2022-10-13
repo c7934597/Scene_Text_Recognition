bb6bcac6db4d9ebe15c503b5e4abc4c779515af6
weiming.lin <weiming.lin@gozeppelin.com>
2022-10-13 19:25:43 +0800

    update

--- deepstream-src-appsrc/OCR/inference.py
+++ deepstream-src-appsrc/OCR/inference.py
@@ -7,7 +7,7 @@ logging.basicConfig(level=logging.INFO, format='%(asctime)s %(message)s', datefm
 logger = logging.getLogger(__name__)
 class Inference:
     def run() -> str:
-        ocr = PaddleOCR(use_angle_cls=False, use_gpu=False, lang="en")  # need to run only once to download and load model into memory
+        ocr = PaddleOCR(use_angle_cls=False, use_gpu=True, lang="en")  # need to run only once to download and load model into memory
         result = ocr.ocr("/opt/nvidia/deepstream/deepstream-6.0/sources/apps/sample_apps/deepstream-src-appsrc/OCR/images/ocr.jpg", cls = False)
         for line in result:
             ocr_string = str.strip(line[1][0])
