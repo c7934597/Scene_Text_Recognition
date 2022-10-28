70bf65f578ed3f28cbd7d2d1bad50e9f1ce5777f
Wei-Ming Lin <c7934597@gmail.com>
2022-10-28 12:39:06 +0800

    update

--- deepstream-src-appsrc/OCR/inference.py
+++ deepstream-src-appsrc/OCR/inference.py
@@ -6,12 +6,13 @@ logger = logging.getLogger(__name__)
 class Inference:
     def run(ocr, image_path) -> str:
         result = ocr.ocr(image_path, cls = False)
-        for line in result:
-            ocr_result = line[0][1][0]
-            if "USDOT" in ocr_result or "US DOT" in ocr_result or "U.S.DOT" in ocr_result or "U.S. DOT" in ocr_result:
-                ocr_result = str.strip(re.sub("[a-zA-Z#.]", "", ocr_result))
-                if 6 <= len(ocr_result) <= 8:
-                    return ocr_result
-                else:
-                    return ""
+        for sub_result in result:
+            for line in sub_result:
+                ocr_result = line[1][0]
+                if "USDOT" in ocr_result or "US DOT" in ocr_result or "U.S.DOT" in ocr_result or "U.S. DOT" in ocr_result:
+                    ocr_result = str.strip(re.sub("[a-zA-Z#-.]", "", ocr_result))
+                    if 6 <= len(ocr_result) <= 8:
+                        return ocr_result
+                    else:
+                        return ""
         return ""
